276a4bc877cd9058bcf38d52fb435509c3853080
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2023-06-10 16:49:05 -0300

    Add executeTemplate function and contact|faq.tmpl

--- main.go
+++ main.go
@@ -7,14 +7,12 @@ import (
 	"net/http"
 	"path/filepath"
 
-	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; chatset=uft-8")
-	tplPath := filepath.Join("templates", "home.tmpl")
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
@@ -28,23 +26,19 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.tmpl")
+	executeTemplate(w, tplPath)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:lucasgrvarela@gmail.com\">lucasgrvarela@gmail.com</a>.</p>")
+	tplPath := filepath.Join("templates", "contact.tmpl")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; chatset=uft-8")
-	fmt.Fprint(w, `<h1>FAQ</h1>
-<ul>
-	<li><b>Is there a free version?</b> Yes! We offer a free trial for 30 days on any paid plans.</li>
-	<li><b>Are you enjoyining?</b> Yes!</li>
-</ul>`)
-}
-
-func userHandler(w http.ResponseWriter, r *http.Request) {
-	userID := chi.URLParam(r, "userID")
-	w.Write([]byte(fmt.Sprintf("hi, %v", userID)))
+	tplPath := filepath.Join("templates", "faq.tmpl")
+	executeTemplate(w, tplPath)
 }
 
 func main() {
@@ -52,11 +46,6 @@ func main() {
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
-
-	userRoutes := r.Group(nil)
-	userRoutes.Use(middleware.Logger)
-	userRoutes.Get("/user/{userID}", userHandler)
-
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	})
