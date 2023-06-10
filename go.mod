aed1b38f2c28c1883cafa51d8d06619febd994fa
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2023-06-10 19:22:46 -0300

    Create views package

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/lucasgrvarela/lenslocked
 
 go 1.20
 
-require (
-	github.com/go-chi/chi v1.5.4
-	github.com/go-chi/chi/v5 v5.0.8
-)
+require github.com/go-chi/chi/v5 v5.0.8
---
+++ views/template.go
@@ -0,0 +1,30 @@
+package views
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Parse(filepath string) (Template, error) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+	return Template{htmlTmpl: tpl}, nil
+}
+
+type Template struct {
+	htmlTmpl *template.Template
+}
+
+func (t Template) Execute(w http.ResponseWriter, data any) {
+	w.Header().Set("Content-Type", "text/html; chatset=uft-8")
+	err := t.htmlTmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
