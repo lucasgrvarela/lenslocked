a5829e6fbcd0d0abf55b4fad521f46dca500f644
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2023-06-21 21:49:26 -0300

    FAQ page with dynamic content

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/lucasgrvarela/lenslocked/views"
@@ -9,5 +10,28 @@ import (
 func StaticHandler(tmpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tmpl views.Template) http.HandlerFunc {
+	faq := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "Are you enjoyining?",
+			Answer:   "Yes!",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us at <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, faq)
 	}
 }
